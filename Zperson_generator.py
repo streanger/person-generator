c184830749d49dcc3b91e9a16b7c22f151eb04d5
streanger <pslisz@o2.pl>
2018-07-30 23:39:08 +0200

    cleaning

--- Zperson_generator.py
+++ Zperson_generator.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 #it would be useful with generate random person data
 #11.02.18 -> it still need a lot of work :(
-import os
-import random
-import sys
-import shutil   #download_image
-import requests #download_image
-import getopt
-import logging
 import csv
+import os
+import getopt
+#import logging
+import random
 import re
+import shutil           #download_image
+import sys
+import tkinter as tk    #gui
+import requests         #download_image
+from PIL import ImageTk, Image
 
 #own modules
 import sqlite_use as sql
 from random_data import get_email, random_date, get_age, random_phone
-
-#gui
-import tkinter as tk
-from PIL import ImageTk, Image
-
 
 def download_flags():
     #just run this function to get flags & countries
@@ -36,11 +33,10 @@ def download_flags():
     gifs = list(set(gifs))
     countries = [item[7:-9].lower() for item in gifs]
     countries.sort()        #in place
-    
-    #write countries to .txt
+
     path = script_path()
-    simple_write("countries.txt", "\n".join(countries))
-    
+    simple_write("countries.txt", "\n".join(countries))     #write countries to .txt
+
     #write gifs to flags dir
     gifsDir = "flags"
     if not os.path.exists(gifsDir):
@@ -48,7 +44,7 @@ def download_flags():
     path = os.path.join(path, gifsDir)
     for gif in gifs:
         gifUrl = flagsUrl + gif
-        gifPath = os.path.join(path, gif[7:])       #need to cut gif subpath
+        gifPath = os.path.join(path, gif[7:])               #need to cut gif subpath
         print(gifUrl)
         download_image(gifUrl, gifPath)
     return gifs, countries
@@ -62,7 +58,7 @@ def download_image(url, fileName="image.png"):
         pass
     del response
     return True
-    
+
 def script_path(fileName=''):
     path = os.path.realpath(os.path.dirname(sys.argv[0]))
     os.chdir(path)  #it seems to be quite important
@@ -78,10 +74,10 @@ def simple_write(file, strContent):
     with open(file, "w") as f:
         f.write(strContent + "\n")
         f.close()
-    return True    
-    
+    return True
+
 def read_file(fileName, rmnl=False):
-    path = os.path.join(PATH, fileName)
+    path = os.path.join(script_path(), fileName)
     try:
         with open(path, "r") as file:
             if rmnl:
@@ -93,15 +89,15 @@ def read_file(fileName, rmnl=False):
     return fileContent
 
 def csv_writer(personList):
-    path = os.path.join(PATH, "persons.csv")
+    path = os.path.join(script_path(), "persons.csv")
     with open(path, "w", newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=",")
         for person in personList:
             writer.writerow(person)
         print("--< person data written to csv file")
     return True
-    
-def write_file(fileName, content, endline="\n", overWrite=False, response=True, rmSign=[]):
+
+def write_file(file_name, content, endline="\n", overWrite=False, response=True):
     if not content:
         return False
     contentType = type(content)
@@ -114,24 +110,24 @@ def write_file(fileName, content, endline="\n", overWrite=False, response=True, 
     else:
         return False
     if overWrite:
-        mode="w"
+        mode = "w"
     else:
-        mode="a"
-    path = os.path.join(PATH, fileName)
+        mode = "a"
+    path = os.path.join(script_path(), file_name)
     with open(path, mode) as file:
         for item in content:
-            if rmSign:
-                for sign in rmSign:
-                    item = (str(item)).replace(sign, "")
+            #if rmSign:
+            #    for sign in rmSign:
+            #        item = (str(item)).replace(sign, "")
             file.writelines(str(item)+endline)
         file.close()
         if response:
-            print("--< written to: {0} | contentType: {1}".format(fileName, contentType))
+            print("--< written to: {0} | contentType: {1}".format(file_name, contentType))
     return True
 
 def write_names(names):
     for key, item in enumerate(names):
-        write_file("name" + str(key) + ".txt", item, addNewline=True, overWrite=True, subPath="")
+        write_file(file_name="name" + str(key) + ".txt", content=item, addNewline=True, overWrite=True, subPath="")
     return 0
 
 def get_data(key, personDictio):
@@ -171,13 +167,14 @@ def get_data(key, personDictio):
     return data
 
 def capitalize_dictio(dictio):
-    for key,val in dictio.items():
+    for key, val in dictio.items():
         if key == "Email":
             continue
-        if type(val) is str:
+        #if type(val) is str:
+        if isinstance(val, str):
             dictio[key] = val.capitalize()
     return dictio
-   
+
 def usage():
     print("usage:")
     print("-u <fileName> - update db with specified file")
@@ -192,16 +189,8 @@ def usage():
     print("-h - this usage help")
     print("--"*35)
     return True
-   
-def generate_person(national="Random", sex="Random", age=0, writeFile=True):
-    personDataDictio = { "Nationality" : "Random",
-                         "Sex" : "Random",
-                         "Name" : "Random",
-                         "Surname" : "Random",
-                         "Birthdate" : "Random",
-                         "Age" : "Random",
-                         "Email" : "Random",
-                         "Phone" : "Random"}
+
+def generate_person(national="Random", sex="Random", age=0):
     personData = {}
     if national == "Random":
         personData["Nationality"] = get_data("Nationality", personData)
@@ -219,19 +208,21 @@ def generate_person(national="Random", sex="Random", age=0, writeFile=True):
     return personData
 
 def show_data(dictio, dataType=0):
+    email = "random"
     if dataType == 0:
-        return "\n".join(dictio.values())
+        email = "\n".join(dictio.values())
     elif dataType == 1:
-        return "\n".join("{}".format(item) for item in dictio.items())
+        email = "\n".join("{}".format(item) for item in dictio.items())
     elif dataType == 2:
-        return "\n".join("{}: {}".format(key, item) for key, item in dictio.items())
+        email = "\n".join("{}: {}".format(key, item) for key, item in dictio.items())
     elif dataType == 3:
-        return [dictio["Name"], dictio["Surname"], dictio["Sex"], dictio["Nationality"], dictio["Birthdate"], dictio["Age"], dictio["Email"], dictio["Phone"]]
+        email = [dictio["Name"], dictio["Surname"], dictio["Sex"], dictio["Nationality"], dictio["Birthdate"], dictio["Age"], dictio["Email"], dictio["Phone"]]
     else:
-        return "\n".join(dictio.values())
+        email = "\n".join(dictio.values())
+    return email
 
 class Application(tk.Frame):
-    def __init__(self, master=None, data=[]):
+    def __init__(self, master, data):
         super().__init__(master)
         self.pack()
         self.data = data
@@ -246,8 +237,8 @@ class Application(tk.Frame):
         self.root = master
         self.create_widgets()
 
-    def create_widgets(self):     
-        self.info_phone = tk.Label(text="Phone: {}".format(self.phone)).pack(expand="yes", fill="both", side="bottom")     
+    def create_widgets(self):
+        self.info_phone = tk.Label(text="Phone: {}".format(self.phone)).pack(expand="yes", fill="both", side="bottom")
         self.info_email = tk.Label(text="Email: {}".format(self.email)).pack(expand="yes", fill="both", side="bottom")
         self.info_age = tk.Label(text="Age: {}".format(self.age)).pack(expand="yes", fill="both", side="bottom")
         self.info_birth = tk.Label(text="Birthdate: {}".format(self.birth)).pack(expand="yes", fill="both", side="bottom")
@@ -266,26 +257,15 @@ class Application(tk.Frame):
         self.read["text"] = "Next"
         self.read["fg"] = "green"
         #self.read["command"] = self.get_post_by_date
-        self.read.pack(side="left")        
+        self.read.pack(side="left")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.root.destroy).pack(side="left")        
-        
-        
-        '''
-        self.scroll = tk.Scrollbar(self)    #self instead of root - it matters
-        self.text = tk.Text(self, height=20, width=20)
-        self.scroll.pack(side="left", fill=tk.Y)
-        self.text.pack()
-        self.scroll.config(command=self.text.yview)
-        self.text.config(yscrollcommand=self.scroll.set)
-        '''
-        
+        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.root.destroy).pack(side="left")
+
         #create image
-        self.image = ImageTk.PhotoImage(Image.open(self.sex + ".png"))    
+        self.image = ImageTk.PhotoImage(Image.open(self.sex + ".png"))
         self.panel = tk.Label(self.root, image=self.image)
-        self.panel.pack(side = "right")
-        
-        
+        self.panel.pack(side="right")
+
 def gui_app(person):        
     root = tk.Tk()
     root.geometry('{}x{}'.format(600, 500))
@@ -296,24 +276,23 @@ def gui_app(person):
 
 def get_opt(argv):
     try:
-        opts, args = getopt.getopt(argv, "hrglu:n:s:q:a:i:")
+        opts, arg = getopt.getopt(argv, "hrglu:n:s:q:a:i:")
     except getopt.GetoptError as err:
         print(str(err))
         return False
-        
+
     #parameters at start
     national = "Random"
     sex = "Random"
     quantity = 1
     age = 0
     gui = False
-    
+
     for opt, arg in opts:
         if opt in "-h":
             usage()
             return False
         elif opt in '-r':
-            #fully random:
             national = "Random"
             sex = "Random"
             #quantity = 1
@@ -331,27 +310,22 @@ def get_opt(argv):
                 status = sql.update_db(arg)
                 if status:
                     print("--< database updated succesfully")
-                    return False
                 else:
                     print("--< failed to update database")
-                    return False
             else:
                 print("no such file: '{}'".format(arg))
-                return False
+            return False
         elif opt in '-i':
             #if arg in os.listdir():
             try:
                 status = sql.update_db(arg, interactive=True)
                 if status:
                     print("--< database updated succesfully")
-                    return False
                 else:
                     print("--< failed to update database")
-                    return False
-            #else:
             except:
                 print("no such file: '{}'".format(arg))
-                return False
+            return False
         elif opt in "-n":
             #nationalList = ["english", "polish", "ukrainian"]
             nationalList = sql.data_from_db("names", "national") + sql.data_from_db("surnames", "national")
@@ -382,18 +356,13 @@ def get_opt(argv):
             usage()
             return False
     return national, sex, quantity, age, gui
-    
+
 def main(argv):
     #check if db exists and
     tables = sql.get_tables("zperson_stuff.db")
     if tables != [("names",), ("surnames",)]:
         print("--< incorrect tables: {}\n--< check if database file exists".format(tables))
         return False
-        
-    #argv = ["-h"]
-    #argv = ["-u", "names.txt"]
-    #argv = ["-r", "-q", "99", "-a", "25"]
-    #argv = ["-n", "polish", "-q", "3", "-a", "22"]
 
     correctOpts = get_opt(argv)
     if correctOpts:
@@ -403,20 +372,20 @@ def main(argv):
         return False
 
     personList = [["Name", "Surname", "Sex", "Nationality", "Birthdate", "Age", "Email", "Phone"]]
-    for x in range(quantity):
-        personData = generate_person(national=national, sex=sex, age=age, writeFile=False)
+    for _ in range(quantity):
+        personData = generate_person(national=national, sex=sex, age=age)
         showPerson = show_data(personData, 2)
-        print("---"*10 + "\n" + showPerson)        
+        print("---"*10 + "\n" + showPerson)
         personList.append(show_data(personData, 3))
 
-        
     #write data to csv
     csv_writer(personList)
     if gui:
         gui_app(personData)           #to show data and flag, map, photo 
 
+
 if __name__ == "__main__":
-    global PATH; PATH = script_path()
+    PATH = script_path()
     main(sys.argv[1:])
 
 
